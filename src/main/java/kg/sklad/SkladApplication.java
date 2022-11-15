80394799f73b79937a25e00bb649f3d128ea67d6
Bekzhan Satiev <tarantuldeveloper@gmail.com>
2022-11-15 10:17:45 +0600

    first commit

---
+++ src/main/java/kg/sklad/SkladApplication.java
@@ -0,0 +1,13 @@
+package kg.sklad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkladApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkladApplication.class, args);
+	}
+
+}
